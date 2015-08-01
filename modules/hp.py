f9d0df6ef4892bb591f76b2353176db769c408af
jahream <jeremy.kang@gatech.edu>
2015-08-01 16:00:29 -0700

    add calculating current health points

--- modules/hp.py
+++ modules/hp.py
@@ -1,5 +1,11 @@
 class Health_Points():
-	def __init__(self, base):
-		self.base = base
-		self.item_bonuses = None
-		self.other_bonuses = None
+    """This class is for health points of the player. Item bonuses
+    and other bonuses are integers. """
+
+    def __init__(self, base):
+        self.base = base
+	self.item_bonuses = None
+	self.other_bonuses = None
+
+    def calc_health(self):
+        return self.base + self.item_bonuses + self.other_bonuses
